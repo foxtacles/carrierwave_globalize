3d81cd52850ce57d9544d664bacd584700c993f8
Christian Semmler <mail@csemmler.com>
2021-07-22 17:32:21 +0200

    fix

--- lib/carrierwave_globalize.rb
+++ lib/carrierwave_globalize.rb
@@ -6,10 +6,11 @@ require 'carrierwave'
 module CarrierwaveGlobalize
   def mount_translated_uploader(column, uploader = nil, options = {}, &block)
     mount_uploader(column, uploader, options, &block)
+    self::Translation.send(:mount_uploader, column, uploader, options, &block)
+
     delegate :"#{column}_will_change!", :"#{column}_changed?",
              to: :translation
 
-    self::Translation.send(:mount_uploader, column, uploader, options, &block)
     return if carrierwave_globalize_initialized?
 
     include CarrierwaveGlobalize::InstanceMethods
@@ -24,28 +25,16 @@ module CarrierwaveGlobalize
   module InstanceMethods
     def self.included(model)
       model.instance_eval do
-        private :_mounter, :_translations_mounter
+        private :_mounter
       end
     end
 
     def _mounter(column)
       if translated_attribute_names.include?(column)
-        _translations_mounter(column)
+        translation.send("_mounter", column)
       else
         super(column)
       end
-    end
-
-    def _translations_mounter(column)
-      @_translations_mounter ||= {}
-      (@_translations_mounter[Globalize.locale.to_sym] ||= {})[column] ||= \
-        if defined?(CarrierWave::Mounter)
-          # CarrierWave >= 1.0.0
-          ::CarrierWave::Mounter.new(self, column)
-        else
-          # CarrierWave < 1.0.0
-          ::CarrierWave::Mount::Mounter.new(self, column)
-        end
     end
   end
 end
